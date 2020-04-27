84e490b9ae94258ece0504a0328b9f3c0bc180b5
Jonathan Peppers <jonathan.peppers@microsoft.com>
2020-04-27 10:57:40 -0500

    File->New project

---
+++ Xamarin.Forms.Benchmarks/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Forms.Benchmarks {
+	public partial class App : Application {
+		public App ()
+		{
+			InitializeComponent ();
+
+			MainPage = new MainPage ();
+		}
+
+		protected override void OnStart ()
+		{
+		}
+
+		protected override void OnSleep ()
+		{
+		}
+
+		protected override void OnResume ()
+		{
+		}
+	}
+}
